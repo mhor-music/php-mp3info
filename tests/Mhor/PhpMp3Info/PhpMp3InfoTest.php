03186abec0697caccdb0fcc3b5393dfa5680917e
Ivo Bathke <ivo.bathke@gmail.com>
2017-12-09 18:14:05 +0100

    updated symfony and dropped php < 5.6 with adjustments

--- tests/Mhor/PhpMp3Info/PhpMp3InfoTest.php
+++ tests/Mhor/PhpMp3Info/PhpMp3InfoTest.php
@@ -18,62 +18,10 @@ class PhpMp3InfoTest extends PHPUnit_Framework_TestCase {
      */
     protected $mp3info;
 
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function createWorkingProcess()
-    {
-        $mockProcess = $this->getMock('Process', array('run', 'getOutput', 'isSuccessful'));
-
-        $mockProcess->expects($this->any())
-            ->method('getOutput')
-            ->will($this->returnValue('ZOE LEELA|Pop Up|1|Digital Guilt|1:44|Variable'));
-
-        $mockProcess->expects($this->any())
-            ->method('isSuccessful')
-            ->will($this->returnValue(true));
-        return $mockProcess;
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function createErrorProcess()
-    {
-        $mockProcess = $this->getMock('Process', array('run', 'isSuccessful', 'getErrorOutput'));
-
-        $mockProcess->expects($this->any())
-            ->method('isSuccessful')
-            ->will($this->returnValue(false));
-
-        return $mockProcess;
-    }
-
-    /**
-     * @param bool $workingProcess
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function createProcessBuilderMock($workingProcess = true)
-    {
-        if ($workingProcess === false) {
-            $mockProcess = $this->createErrorProcess();
-        } else {
-            $mockProcess = $this->createWorkingProcess();
-        }
-
-        $mock = $this->getMock('ProcessBuilder', array('add', 'getProcess', 'setArguments'));
-
-        $mock->expects($this->any())
-            ->method('getProcess')
-            ->will($this->returnValue($mockProcess));
-
-        return $mock;
-    }
-
     public function testTags()
     {
         $this->filePath =  __DIR__ . '/../../testFiles/ZOE.LEELA_-_Pop_Up.mp3';
-        $this->mp3info = new PhpMp3Info($this->createProcessBuilderMock(true));
+        $this->mp3info = new PhpMp3Info();
         $mp3tags = $this->mp3info->extractId3Tags($this->filePath);
 
         $this->assertEquals('Digital Guilt', $mp3tags->getAlbum());
@@ -90,8 +38,9 @@ class PhpMp3InfoTest extends PHPUnit_Framework_TestCase {
      */
     public function testFailCommand()
     {
+        $process = $this->prophesize('Process');
         $this->filePath =  __DIR__ . '/../../testFiles/ZOE.LEELA_-_Pop_Up.mp3';
-        $mp3tags = new PhpMp3Info($this->createProcessBuilderMock(false));
+        $mp3tags = new PhpMp3Info($process->reveal());
         $mp3tags->extractId3Tags($this->filePath);
     }
 
@@ -106,4 +55,3 @@ class PhpMp3InfoTest extends PHPUnit_Framework_TestCase {
     }
 
 }
- 
